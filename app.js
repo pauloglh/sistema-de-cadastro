72d4e9a8235d209afe37aa42f1ed858886c9e8b5
Paulo Guilherme <130486478+pauloglh@users.noreply.github.com>
2023-10-18 21:08:47 -0300

    Adicionando mensagem extra de teste

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log ("Rodando o sistema de gerenciamento de jogos")
 console.log ("Aplicação esta se conectando ao banco de dados")
 //Criando edição
 console.log ("Criando mensagem para teste")
+console.log ("Adicionando mais uma mensagem de teste")
