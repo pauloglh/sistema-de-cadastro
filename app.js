b25db858ec32668866ad6e1d44d63ab11e800d20
Paulo Guilherme <130486478+pauloglh@users.noreply.github.com>
2023-10-18 21:06:25 -0300

    atualizando arquivo js com mensagem de teste

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log ("Rodando o sistema de gerenciamento de jogos")
 console.log ("Aplicação esta se conectando ao banco de dados")
+//Criando edição
+console.log ("Criando mensagem para teste")
